dc5f4a430170b5d8b2592195ee45d1d0ac12d161
Tom Christie <tom@tomchristie.com>
2021-08-09 15:17:50 +0100

    Add analysis and README

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 # Top 1000 URLs
 
-Starting point... https://gtmetrix.com/top1000.html
+I've taken a dataset of top URLs from https://gtmetrix.com/top1000.html
+
+* Ran `download.py` to download and save the content and headers.
+* Then ran `analysis.py` to determine the likely character encodings.
+
+```text
+Total pages: 855
+
+Total pages with a specified charset:                                               73.1%
+Total pages with no specified charset, which successfully decode as utf-8:          22.5%
+Total pages with no specified charset, which result in a successful chardet guess:   4.4%
+Total pages with no specified charset, and an unsuccessful chardet guess:            0.0%
+Total pages with no specified charset, and chardet returned no guess at all:         0.0%
+```
+
+Based on the results I've seen here, I think the most sensible policy for HTTPX to use
+for charcter decoding would be...
+
+* Default to whatever charset is specified, using `errors='replace'`.
+* Failing that, attempt `.decode(utf-8, errors='strict')`. Much quicker than chardet, and handles the large majority of cases.
+* Failing that, attempt chardet.
+
+Also, it's important that the Chinese character set GB2312 should always be coerced to the superset GBK8,
+since that resolves a bunch of cases that'll otherwise fail. This is important both for cases with a specified charset, and for cases where we guess with chardet.
+
+```
+Specified charsets:
+utf-8 598
+gbk 8
+iso-8859-1 6
+shift_jis 4
+euc-jp 2
+iso-8859-15 2
+utf8 1
+iso-8859-2 1
+euc-kr 1
+windows-1251 1
+windows-1256 1
+
+Guessed charsets:
+gbk 21
+shift_jis 8
+euc-kr 5
+windows-1252 2
+iso-8859-1 1
+euc-jp 1
+```
---
+++ analysis.py
@@ -0,0 +1,96 @@
+import codecs
+import chardet
+import cgi
+import httpx
+import os
+from collections import defaultdict
+
+
+def charset_encoding(headers):
+    content_type = headers.get("Content-Type")
+    if content_type is None:
+        return None
+
+    _, params = cgi.parse_header(content_type)
+    if "charset" not in params:
+        return None
+
+    encoding = params["charset"].strip("'\"").lower()
+    if encoding == "gb2312":
+        encoding = "gbk"
+    try:
+        codecs.lookup(encoding)
+    except LookupError:
+        return None
+    return encoding
+
+
+def decodes_as(content, encoding):
+    try:
+        content.decode(encoding, errors='strict')
+    except UnicodeDecodeError:
+        return False
+    return True
+
+
+def chardet_guess(content):
+    best_guess = chardet.detect(content)['encoding']
+    if best_guess is not None:
+        best_guess = best_guess.lower()
+    if best_guess == 'gb2312':
+        return "gbk"
+    return best_guess
+
+
+total = 0
+total_with_specified_encoding = 0
+total_with_utf8 = 0
+total_with_successful_chardet_guess = 0
+total_with_unsuccessful_chardet_guess = 0
+total_with_unknown = 0
+
+specified_charsets = defaultdict(int)
+guessed_charsets = defaultdict(int)
+
+for idx in range(1000):
+    if not os.path.exists(f"content/{idx}"):
+        continue
+
+    with open(f"content/{idx}", "rb") as content_file:
+        content = content_file.read()
+    with open(f"headers/{idx}", "r") as headers_file:
+        headers = httpx.Headers(eval(headers_file.read()))
+
+    total += 1
+    specified_encoding = charset_encoding(headers)
+    if specified_encoding:
+        total_with_specified_encoding += 1
+        specified_charsets[specified_encoding] += 1
+    else:
+        if decodes_as(content, "utf-8"):
+            total_with_utf8 += 1
+        else:
+            guess_encoding = chardet_guess(content)
+            if guess_encoding:
+                if decodes_as(content, guess_encoding):
+                    guessed_charsets[guess_encoding] += 1
+                    total_with_successful_chardet_guess += 1
+                else:
+                    total_with_unsuccessful_chardet_guess += 1
+            else:
+                total_with_unknown += 1
+
+print("Total: ", total)
+print("Total specified:          %.1f%%" % (100.0*total_with_specified_encoding/total))
+print("Total utf-8:              %.1f%%" % (100.0*total_with_utf8/total))
+print("Total successful guess:   %.1f%%" % (100.0*total_with_successful_chardet_guess/total))
+print("Total unsuccessful guess: %.1f%%" % (100.0*total_with_unsuccessful_chardet_guess/total))
+print("Total unknown:            %.1f%%" % (100.0*total_with_unknown/total))
+print()
+print("Specified charsets:")
+for item in sorted(specified_charsets.items(), key=lambda i: i[1], reverse=True):
+    print(item[0], item[1])
+print()
+print("Guessed charsets:")
+for item in sorted(guessed_charsets.items(), key=lambda i: i[1], reverse=True):
+    print(item[0], item[1])
