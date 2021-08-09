4bd96b8d55f70cb57ea14cf99b4ef00aed05f1a0
Tom Christie <tom@tomchristie.com>
2021-08-09 13:11:14 +0100

    Add download.py file

---
+++ download.py
@@ -0,0 +1,19 @@
+import csv
+import httpx
+
+
+with open("top1000.txt") as top1000:
+    for row_count, line in enumerate(top1000.readlines()):
+        url = line.split()[1]
+        #print(row_count, url)
+        try:
+            r = httpx.get(url)
+        except httpx.RequestError as exc:
+            print(row_count, url, exc)
+        else:
+            print(row_count, url, r.status_code)
+
+        with open(f"content/{row_count}", "wb") as content_file:
+            content_file.write(r.content)
+        with open(f"headers/{row_count}", "w") as headers_file:
+            headers_file.write(repr(r.headers.raw))
